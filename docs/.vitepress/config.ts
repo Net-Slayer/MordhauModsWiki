80342ef6131eee08ec8e3903f2250c3f66f9fc6f
Net-Slayer <14405930+Net-Slayer@users.noreply.github.com>
2023-08-02 00:28:14 +0100

    + Addition of automatic sidebar generation * fixed readme.md having duplicate attributes from gitbook format + Added node module for decapcms * fixed gitignore to avoid tracking vitepress cache

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vitepress'
+import { generateSidebar } from 'vitepress-sidebar'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Mordhau Mods Wiki",
+  description: "A community modding wiki for mordhau",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: generateSidebar({
+		documentRootPath: '/docs',
+		collapseDepth: 2,
+		hyphenToSpace: true,
+		capitalizeFirst: true,
+		sortByFileName: [
+			"Introduction", 
+			"Getting Started", 
+			"Maps", 
+			"Gamemodes", 
+			"Custom Content",
+			"Advanced topics",
+			"Community Resources"
+		]
+		}),
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Mordhau Mods Wiki"
+  text: "A community modding wiki for mordhau"
+  tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: Modding
+      link: /introduction
+    - theme: alt
+      text: Server Hosting
+      link: /server-hosting
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
