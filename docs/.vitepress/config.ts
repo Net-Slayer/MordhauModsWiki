0f3307851b6495bbf40e766310e82f09f97dd9ff
Net-Slayer <14405930+Net-Slayer@users.noreply.github.com>
2023-08-04 15:22:05 +0100

    + Added vue as peerDependency - Removed ssr option, no changes to build error * Re-enabled sorting in sidebar plugin for use in dev mode

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,35 +1,38 @@
 import { defineConfig } from 'vitepress'
-//import { generateSidebar } from 'vitepress-sidebar'
+import { generateSidebar } from 'vitepress-sidebar'
+
 
 // https://vitepress.dev/reference/site-config
 export default ({
   title: "Mordhau Mods Wiki",
   description: "A community modding wiki for mordhau",
+    // ssr: {
+      // noExternal: ['vitepress-sidebar']
+
+    // },  
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Examples', link: '/markdown-examples' }
     ],
-
-    // sidebar: generateSidebar({
-		// documentRootPath: '/docs',
-		// collapseDepth: 2,
-		// hyphenToSpace: true,
-		// capitalizeFirst: true,
-		// sortByFileName: [
-			// "Introduction", 
-			// "Getting Started", 
-			// "Maps", 
-			// "Gamemodes", 
-			// "Custom Content",
-			// "Advanced topics",
-			// "Community Resources"
-		// ]
-		// }),
-
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+    ],
+    sidebar: generateSidebar({
+		documentRootPath: '/docs',
+		collapseDepth: 2,
+		hyphenToSpace: true,
+		capitalizeFirst: true,
+		sortByFileName: [
+			"Introduction", 
+			"Getting Started", 
+			"Maps", 
+			"Gamemodes", 
+			"Custom Content",
+			"Advanced topics",
+			"Community Resources"
+		]
+		}),
   }
 })
